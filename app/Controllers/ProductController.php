82de2351c210f058c7c904bb730e833dd3dc41e9
Bernadeth Bautista <badethbautista07@gmail.com>
2023-09-25 08:32:43 +0800

    code

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -3,17 +3,61 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-
+use App\Models\ProductModel; // Uncomment this line to import the ProductModel class
 
 class ProductController extends BaseController
 {
+    private $product;
+
+    public function __construct()
+    {
+        $this->product = new ProductModel(); // Change the instantiation to use the imported namespace
+    }
+
+    // ... (rest of your methods)
+
+
+    public function delete($id)
+    {
+        $product = new ProductModel(); 
+        $this->product->delete($id);
+        return redirect()->to('/product');
+    }
+    public function edit($id)
+    {
+        $product = new ProductModel();
+        $data = [
+            'product' => $this->product->findAll(),
+            'pro' => $this->product->where('id', $id)->first(),
+        ];
+        return view('products', $data);
+    }
+    public function save()
+    {
+        $id = $_POST['id'];
+        $data = [
+            'id' => $this->request->getVar('id'),
+            'ProductName' => $this->request->getVar('ProductName'),
+            'ProductDescription' => $this->request->getVar('ProductDescription'),
+            'ProductCategory' => $this->request->getVar('ProductCategory'),
+            'ProductQuantity' => $this->request->getVar('ProductQuantity'),
+            'ProductPrice' => $this->request->getVar('ProductPrice'),
+          ];
+          $product = new ProductModel();
+        if( $id != null){
+            $this->product->where('id', $_POST['id'])->edit($data);
+        }else{
+            return redirect()->to('/product');
+        }
+    }
     public function product($product)
     {
         echo $product;
     }
     public function badeth()
     {
-        return view('products');
+        $data['product'] = $this->product->findAll();
+        return view('products', $data);
     }
     public function index()
     {
--- app/Views/products.php
+++ app/Views/products.php
@@ -5,6 +5,53 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="/save" method="post">
+            <label>id</label>
+            <input type = "hidden" name = "id" value = "<?= isset($pro['id']) ?>">
+            <input type = "text" name = "ProductId" placeholder="ProductId" value = "<?= isset($pro['id']) ?>">
+            <br><br>
+            <label>ProductName</label>
+            <input type = "text" name = "ProductName" placeholder="name" value = "<?= isset($pro['ProductName']) ?>">
+            <br><br>
+            <label>ProductDescription</label>
+            <input type = "text" name = "ProductDescription" placeholder="description" value = "<?= isset($pro['ProductDescription']) ?>">
+            <br><br>
+            <label>ProductCategory</label>
+            <input type = "text" name = "ProductCategory" placeholder="category" value = "<?= isset($pro['ProductCategory']) ?>">
+            <br><br>
+            <label>ProductQuantity</label>
+            <input type = "number" name = "ProductQuantity" placeholder="quantity" value = "<?= isset($pro['ProductQuantity']) ?>">
+            <br><br>
+            <label>ProductPrice</label>
+            <input type = "number" name = "ProductPrice" placeholder="price" value = "<?= isset($pro['ProductPrice']) ?>">
+            <br><br>
+            <input type="submit" value="save">
+    </form>
     <h1>Products</h1>
+    <table border ="1">
+        <tr>
+            <th>id</th>
+            <th>ProductName</th>
+            <th>ProductDescription</th>
+            <th>ProductCategory</th>
+            <th>ProductQuantity</th>
+            <th>ProductPrice</th>
+            <th>Action</th>
+            <th>Action</th>
+        </tr>
+        <?php foreach ($product as $pr): ?>
+            <tr>
+                <td><?= $pr['id'] ?> </td>
+                <td><?= $pr['ProductName'] ?> </td>
+                <td><?= $pr['ProductDescription'] ?> </td>
+                <td><?= $pr['ProductCategory'] ?> </td>
+                <td><?= $pr['ProductQuantity'] ?> </td>
+                <td><?= $pr['ProductPrice'] ?> </td>
+                <td><a href="/delete/<?= $pr['id'] ?>">Delete</a> </td>
+                <td><a href="/edit/<?= $pr['id'] ?>">Edit</a> </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
 </body>
 </html>
